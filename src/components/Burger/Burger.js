a70dc76a4d6775758d07472f75a4a0b5fa3da637
massimo <massimo@info.nl>
2019-08-10 11:07:09 +0200

    Outputting Burger Ingredients Dynamically part 2

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,16 +3,24 @@ import styles from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
-        .map(igKey => {
-            return [...Array(props.ingredients[igKey])].map((_, i) => {
-               return <BurgerIngredient key={igKey + i} type={igKey} />
-            })
+    // const transformedIngredients = Object.keys(props.ingredients)
+    //     .map(igKey => {
+    //         return [...Array(props.ingredients[igKey])].map((_, i) => {
+    //            return <BurgerIngredient key={igKey + i} type={igKey} />
+    //         })
+    //     });
+
+    const transformedIngredientsKeyValues = Object.entries(props.ingredients)
+        .map(([key, val]) => {
+            const indexes = [...Array(val).keys()];
+            return indexes.map( i=> {
+                return <BurgerIngredient key={key + i} type={key} />
+            });
         });
     return (
         <div className={styles.Burger}>
             <BurgerIngredient type="bread-top" />
-                {transformedIngredients}
+                {transformedIngredientsKeyValues}
             <BurgerIngredient type="bread-bottom" />
 
         </div>
